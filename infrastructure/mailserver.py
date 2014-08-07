a4b4eeb5d0f7c51a789bf094b0d2fd822d7767d8
Liso Gallo <liso@riseup.net>
2014-08-07 10:51:40 -0300

    [IMP] mailserver model migrated to API v8

--- infrastructure/mailserver.py
+++ infrastructure/mailserver.py
@@ -1,51 +1,14 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    Infrastructure
-#    Copyright (C) 2014 Ingenieria ADHOC
-#    No email
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+
+from openerp import models
 
 
-import re
-from openerp import netsvc
-from openerp.osv import osv, fields
-
-class mailserver(osv.osv):
+class mailserver(models.Model):
     """"""
-    
+
     _name = 'infrastructure.mailserver'
     _description = 'mailserver'
-    _inherits = {  }
-    _inherit = [ 'ir.mail_server' ]
-
-    _columns = {
-    }
-
-    _defaults = {
-    }
-
-
-    _constraints = [
-    ]
-
-
+    _inherit = ['ir.mail_server']
 
 
 mailserver()
-
-# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
