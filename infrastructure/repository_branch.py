ed05a2b93fff8fbab93d6da2ddf32d1f26497ee1
Liso Gallo <liso@riseup.net>
2014-08-07 11:49:42 -0300

    [IMP] repository_branch model migrated to API v8

--- infrastructure/repository_branch.py
+++ infrastructure/repository_branch.py
@@ -1,51 +1,26 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    Infrastructure
-#    Copyright (C) 2014 Ingenieria ADHOC
-#    No email
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+
+from openerp import models, fields
 
 
-import re
-from openerp import netsvc
-from openerp.osv import osv, fields
-
-class repository_branch(osv.osv):
+class repository_branch(models.Model):
     """"""
-    
+
     _name = 'infrastructure.repository_branch'
     _description = 'repository_branch'
 
-    _columns = {
-        'name': fields.char(string='Name', required=True),
-        'repository_ids': fields.many2many('infrastructure.repository', 'infrastructure_repository_ids_branch_ids_rel', 'repository_branch_id', 'repository_id', string='repository_ids'), 
-    }
+    name = fields.Char(
+        string='Name',
+        required=True
+    )
 
-    _defaults = {
-    }
-
-
-    _constraints = [
-    ]
-
-
+    repository_ids = fields.Many2many(
+        'infrastructure.repository',
+        'infrastructure_repository_ids_branch_ids_rel',
+        'repository_branch_id',
+        'repository_id',
+        string='repository_ids'
+    )
 
 
 repository_branch()
-
-# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
