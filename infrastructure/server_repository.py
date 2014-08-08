15ddeeaf29857e8eec0590541b0eaffd14ffad34
Liso Gallo <liso@riseup.net>
2014-08-08 14:41:04 -0300

    [IMP] server_repository model migrated to API v8

--- infrastructure/server_repository.py
+++ infrastructure/server_repository.py
@@ -1,52 +1,74 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    Infrastructure
-#    Copyright (C) 2014 Ingenieria ADHOC
-#    No email
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+
+from openerp import models, fields, api, _
+from openerp.exceptions import except_orm
+from fabric.api import cd, sudo
+from fabric.contrib.files import exists
+import os
 
 
-import re
-from openerp import netsvc
-from openerp.osv import osv, fields
-
-class server_repository(osv.osv):
+class server_repository(models.Model):
     """"""
-    
+
     _name = 'infrastructure.server_repository'
     _description = 'server_repository'
 
-    _columns = {
-        'repository_id': fields.many2one('infrastructure.repository', string='Repository', required=True),
-        'path': fields.char(string='Path'),
-        'server_id': fields.many2one('infrastructure.server', string='server_id', ondelete='cascade', required=True), 
-    }
+    repository_id = fields.Many2one(
+        'infrastructure.repository',
+        string='Repository',
+        required=True
+    )
 
-    _defaults = {
-    }
+    path = fields.Char(
+        string='Path'
+    )
 
+    server_id = fields.Many2one(
+        'infrastructure.server',
+        string='server_id',
+        ondelete='cascade',
+        required=True
+    )
 
-    _constraints = [
-    ]
+    @api.one
+    def get_repository(self):
+        print 'Getting repository'
+        self.path = self.repository_id.get_repository(self.server_id)[0]
 
+    @api.one
+    def update_repository(self, path=False):
+        print 'Updating repository'
+        self.server_id.get_env()
+        if not path:
+            path = self.path
+        if not path and not exists(path, use_sudo=True):
+            raise except_orm(_('No Repository Folder!'), _(
+                "Please check that the setted path exists or empty \
+                it in order to donwload for first time '%s'!") % (path,))
 
+        with cd(path):
+            try:
+                sudo('git pull')
+            except:
+                raise except_orm(_('Error Making git pull!'), _(
+                    "Error making git pull on '%s'!") % (path))
+
+    @api.one
+    def get_update_repository(self):
+        self.server_id.get_env()
+        if not self.path:
+            # Check if repository on path
+            path = os.path.join(
+                self.server_id.sources_folder, self.repository_id.folder)
+            if exists(path, use_sudo=True):
+                # aparentemente ya existe el repo, vamos a intentar actualizarlo
+                self.update_repository(path)
+                self.path = path
+            else:
+                self.get_repository()
+        else:
+            self.update_repository()
+        return True
 
 
 server_repository()
-
-# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
