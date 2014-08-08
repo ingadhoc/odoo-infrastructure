a89c3e68ff2b9594c0e8206475c77aaecebe5df7
Liso Gallo <liso@riseup.net>
2014-08-08 14:20:54 -0300

    [REM] Removed old server_configuration_command.py file

--- infrastructure_x/__init__.py
+++ infrastructure_x/__init__.py
@@ -1,11 +1,5 @@
 # -*- coding: utf-8 -*-
 
 import wizard
-import non_safe_eval
-import server_configuration
-import server
-import repository
 import server_repository
-import server_configuration_command
-import environment_repository
 import server_hostname
