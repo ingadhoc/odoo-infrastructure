a020d28fbca4da6fecfaf85faf9ccbbeddd6822f
Juan Jose Scarafia <scarafia.juanjose@gmail.com>
2015-05-14 18:15:14 -0300

    IMP move .py files to models folder

---
+++ infrastructure/models/__init__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+import command
+import repository
+import repository_branch
+import odoo_version
+import base_module
+import database
+import database_type
+import database_backup
+import database_module
+import database_user
+import db_filter
+import environment
+import instance
+import instance_repository
+import instance_host
+import mailserver
+import server
+import server_change
+import server_configuration
+import server_configuration_command
+import server_hostname
+import docker_image
+import server_docker_image
