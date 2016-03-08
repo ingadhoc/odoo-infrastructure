df48f6b57dd2fff8e22c0df95247afb0a630dd76
Damian Soriano <damiansoriano@gmail.com>
2016-03-08 23:05:28 +0000

    Create context manager to lock on ir.config_paramter record

---
+++ infrastructure/utils/utils.py
@@ -0,0 +1,24 @@
+import contextlib
+
+from openerp import exceptions
+
+
+@contextlib.contextmanager
+def synchronize_on_config_parameter(env, parameter):
+    param_model = env['ir.config_parameter']
+    param = param_model.search([('key', '=', parameter)])
+    if param:
+        try:
+            env.cr.execute(
+                '''select *
+                    from ir_config_parameter
+                    where id = %s
+                    for update nowait''',
+                param.id,
+                log_exceptions=False
+            )
+        except psycopg2.OperationalError, e:
+            raise exceptions.UserError(
+                'Cannot synchronize access. Another process lock the parameter'
+            )
+    yield
