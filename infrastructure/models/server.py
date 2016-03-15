ddea48e6f647473f6e705152b69eb0f9d87aef22
Juan Jose Scarafia <jjs@adhoc.com.ar>
2016-03-15 17:43:49 -0300

    FIX synchronize_on_config_parameter

--- infrastructure/models/server.py
+++ infrastructure/models/server.py
@@ -4,7 +4,7 @@
 # directory
 ##############################################################################
 from openerp import models, fields, api, _
-from openerp.addons.infrastructure.utils import utils
+# from openerp.addons.infrastructure.utils import utils
 from openerp.exceptions import except_orm, Warning
 from fabric.api import env, reboot
 from fabric.contrib.files import append, upload_template
@@ -16,10 +16,31 @@ import socket
 import sys
 import os
 import logging
+import psycopg2
+from openerp import exceptions
 _logger = logging.getLogger(__name__)
 
 
+def synchronize_on_config_parameter(env, parameter):
+    param_model = env['ir.config_parameter']
+    param = param_model.search([('key', '=', parameter)], limit=1)
+    if param:
+        try:
+            env.cr.execute(
+                """select *
+                    from ir_config_parameter
+                    where id = %s
+                    for update nowait""" %
+                param.id
+            )
+        except psycopg2.OperationalError, e:
+            raise exceptions.UserError(
+                'Cannot synchronize access. Another process lock the parameter'
+                'This is what we get: %s' % e
+            )
+
 FABRIC_LOCKING_PARAMETER = 'fabric_lock'
+
 
 # TODO deberiamos cambiar esto por los metodos propios de fabtools para
 # gestionar errores asi tmb, por ejemplo, lo toma fabtools y otros comandos
@@ -425,7 +446,7 @@ class server(models.Model):
     @api.multi
     def get_env(self):
         # TODO ver si usamos env.keepalive = True para timeouts the nginx ()
-        utils.synchronize_on_config_parameter(
+        synchronize_on_config_parameter(
             self.env, FABRIC_LOCKING_PARAMETER
         )
         self.ensure_one()
