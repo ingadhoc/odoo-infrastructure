7b4f9ee59b68d1bf022a67542d0b41c09b9d8585
Juan Jose Scarafia <jjs@adhoc.com.ar>
2016-09-15 13:02:24 -0300

    IMP make installable

--- infrastructure/__openerp__.py
+++ infrastructure/__openerp__.py
@@ -81,7 +81,7 @@
         'data/email_template.xml',
         'security/ir.model.access.csv',
     ],
-    'installable': False,
+    'installable': True,
     'auto_install': False,
     'application': True,
     'demo': [
