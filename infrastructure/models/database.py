b183b41e84de9e72eca1ab59609229312aa5b47f
Juan Jose Scarafia <jjs@adhoc.com.ar>
2016-12-16 20:49:57 -0300

    IMP

--- infrastructure/models/database.py
+++ infrastructure/models/database.py
@@ -1336,7 +1336,8 @@ class database(models.Model):
         self.server_id.get_env()
         client = self.get_client()
 
-        modules = ['auth_server_admin_passwd_passkey', 'mail']
+        modules = ['crm', 'mail']
+        # modules = ['auth_server_admin_passwd_passkey', 'mail']
         for module in modules:
             if client.modules(name=module, installed=True) is None:
                 raise ValidationError(_(
